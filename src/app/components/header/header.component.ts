53a46140f8e46d8b02fff4f5b03fea905bcbdc28
Josiassantos1577 <josiassantos1577@gmail.com>
2025-02-02 19:38:08 -0300

    refactor: Componentizando header

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,16 @@
+import {Component, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  @Output() showModal = new EventEmitter<void>();
+
+  onShowModal() {
+    this.showModal.emit();
+  }
+}

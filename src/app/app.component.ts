7ec8cd5d4058bb968a814b457ed3cb043760795a
Josiassantos1577 <josiassantos1577@gmail.com>
2025-02-01 22:11:45 -0300

    feat: Adicionando funcionalidades da página

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,45 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatIconModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'cafeteria';
+  @ViewChild('modal') modal!: ElementRef;
+  @ViewChild('mascara') mascara!: ElementRef;
+
+  ngAfterViewInit(): void {
+    console.log('This.modal is already defined: ', this.modal);
+  }
+
+  mostrarModal() {
+    this.modal.nativeElement.style.left = '50%';
+    this.mascara.nativeElement.style.visibility = 'visible';
+  }
+
+  fecharModal() {
+    this.modal.nativeElement.style.left = '-30%';
+    this.mascara.nativeElement.style.visibility = 'hidden';
+  }
+
+  @HostListener('window:scroll', [])
+  onScroll() {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    const button = document.getElementById('totop');
+
+    if (scrollTop > 300) {
+      button?.classList.add('on--totop');
+    } else {
+      button?.classList.remove('on--totop');
+    }
+  }
 }
